767534a19c313903f4de9e1d1da803e8571087dd
Jacob Pecile <jacob.pecile@gmail.com>
2017-01-27 14:15:03 -0500

    added billboard api and express to server

---
+++ Server/restFunctions.js
@@ -0,0 +1,1 @@
+//RESTful functions will go here
--- Server/test.js
+++ Server/test.js
@@ -1,1 +1,15 @@
 console.log('Node is installed!');
+
+var billboard = require("billboard-top-100").getChart;
+ 
+// date defaults to saturday of this week 
+ 
+billboard('hot-100', '2016-01-02', function(songs, err){
+    if (err) console.log(err);
+    //console.log(songs); //prints array of top 100 songs 
+    console.log(songs[3]); //prints song with rank: 4 
+    console.log(songs[0].title); //prints title of top song 
+    console.log(songs[0].artist); //prints artist of top songs 
+    console.log(songs[0].rank) //prints rank of top song (1) 
+    console.log(songs[0].cover) //prints URL for Billboard cover image of top song 
+});
